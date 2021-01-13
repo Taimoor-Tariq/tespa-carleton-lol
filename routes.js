5359c445569664475a0f8db96d4c4470407eb7c9
Taimoor-Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2021-01-12 20:01:56 -0500

    Create Matches Page

--- routes.js
+++ routes.js
@@ -8,5 +8,12 @@ router.get('/', async (req, res) => {
         style: sass.renderSync({file: `${__dirname}/scss/home.scss`}).css.toString(),
     }));
 });
-    
+
+router.get('/matches', async (req, res) => {
+    res.send(pug.renderFile(`${__dirname}/views/matches.pug`, {
+        style: sass.renderSync({file: `${__dirname}/scss/matches.scss`}).css.toString(),
+        GAMES: []
+    }));
+});
+
 module.exports = router;
